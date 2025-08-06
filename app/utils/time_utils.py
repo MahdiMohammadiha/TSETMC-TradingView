de56f38edf19b833210532d016f4aa62281ce192
Mahdi-Mohamadiha <mahdi.mohamadiha.znu@gmail.com>
2025-08-06 10:57:02 +0330

    Interval timestamp calculator

---
+++ app/utils/time_utils.py
@@ -0,0 +1,16 @@
+def round_timestamp(ts: int, interval: str = "1m") -> int:
+    """
+    Round a timestamp (in milliseconds) to the beginning of the interval.
+    """
+    ms = {
+        "1m": 60_000,
+        "5m": 5 * 60_000,
+        "15m": 15 * 60_000,
+        "30m": 30 * 60_000,
+        "1h": 60 * 60_000,
+    }.get(interval)
+
+    if ms is None:
+        raise ValueError(f"Unsupported interval: {interval}")
+
+    return ts - (ts % ms)
