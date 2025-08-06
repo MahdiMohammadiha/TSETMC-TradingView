ea0e44437f985221023342a18f60495696decd2d
Mahdi-Mohamadiha <mahdi.mohamadiha.znu@gmail.com>
2025-08-06 10:49:11 +0330

    Module for generate and update trading view standard candles

---
+++ app/services/aggregator.py
@@ -0,0 +1,33 @@
+from app.utils.time_utils import round_timestamp
+from app.db.mongo import get_db
+
+
+async def update_candles_from_trade(trade, intervals=["1m"]):
+    print(f"💡 update_candles_from_trade called for inscode: {trade.get('InsCode')}")
+
+    price = trade["PDrCotVal"]
+    volume = trade["QTotTran5J"]
+    symbol = str(trade["InsCode"])
+    timestamp = trade["received_at"]
+
+    for interval in intervals:
+        candle_time = round_timestamp(timestamp, interval)
+        filter = {"symbol": symbol, "interval": interval, "time": candle_time}
+        update = {
+            "$min": {"low": price},
+            "$max": {"high": price},
+            "$set": {
+                "close": price,
+                "volume": volume,
+                "last_updated": timestamp,
+            },
+            "$setOnInsert": {
+                "open": price,
+                "time": candle_time,
+                "symbol": symbol,
+                "interval": interval,
+            },
+        }
+
+        db = get_db()
+        await db["candles"].update_one(filter, update, upsert=True)
